efef65be61a5d7b6c60630aa3b0c7adc1462971c
annasnurdin <anasnurdin02@gmail.com>
2026-05-29 14:30:16 +0700

    tipe data string

---
+++ src/TipeDataString.java
@@ -0,0 +1,13 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Annas";
+        String lastName = "Nurdin";
+        // string xxx = 'sxx';     <== ini salah karena 's' nya kecil dan pakai petik satu
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        String fullname = firstName + " " + lastName;
+        System.out.println(fullname);
+    }
+}
