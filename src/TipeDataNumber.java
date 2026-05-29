0f1f041bbb6f88728ebe8fad77109ea72025fe46
annasnurdin <anasnurdin02@gmail.com>
2026-05-29 14:16:57 +0700

    tipe data number

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,24 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+
+        // bilangan bulat
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 123;
+        long iniLong = 100000000;
+        long iniLong2 = 1000L;
+
+        // bilangan desimal
+        float iniFloat = 10.10f; // bisa pakai 10.10F besar
+        double iniDouble = 12.1212;
+
+        // literal
+        int decimal = 12;
+        int hexDecimal = 0x1232;
+        int binaryInt = 0b101010;
+
+        // ada underscore
+        long balance = 1000_000_000; // biar mudah saja, bebas letaknya
+
+    }
+}
