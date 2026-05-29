cf4aba9c52c27d7ed7f351a5c4c66f268fae5afc
annasnurdin <anasnurdin02@gmail.com>
2026-05-29 14:21:36 +0700

    tipe data char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,13 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char a = 'A';
+        char n = 'N';
+        char s = 'S'; // char pakai petik satu
+
+        System.out.print(a);
+        System.out.print(n);
+        System.out.print(n);
+        System.out.print(a);
+        System.out.print(s);
+    }
+}
