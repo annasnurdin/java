d4984fb820551b75266336ae1cc1fe3850c993d2
annasnurdin <anasnurdin02@gmail.com>
2026-06-02 07:10:38 +0700

    variable

---
+++ src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Annas Nurdin"; // define variabel bisa terpisah dengan pengisian value
+
+        System.out.println(name);
+
+        var nama = "Annas Aja"; // kalau var, harus sebaris deklarasi dan isi value;
+        System.out.println(nama);
+
+        final String sukses = "Ini variabel yang final, tidak bisa diubah, seperti const";
+        System.out.println(sukses);
+        
+    }
+}
