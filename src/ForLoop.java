e40fe549b27d851cbfc14517de7a156896b03d29
annasnurdin <anasnurdin02@gmail.com>
2026-06-26 10:49:39 +0700

    counting loop

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -2,7 +2,7 @@ public class ForLoop {
     public static void main(String[] args) {
         var counter = 0;
 
-        for (;;){
+        for (;counter <=10;){
             System.out.println("Perulangan" + counter);
             counter++;
         }
